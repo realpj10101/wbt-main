ef236ee9ab7ec9b0cfd78a735b0896b1f290521e
Parsa Jafari <Parsajfr1385oo@gmail.com>
2024-10-27 13:04:21 +0330

    create register player service

--- client/src/app/services/register-player.service.ts
+++ client/src/app/services/register-player.service.ts
@@ -17,4 +17,98 @@ export class RegisterPlayerService {
   http = inject(HttpClient);
   router = inject(Router);
   platformId = inject(PLATFORM_ID);
+
+  private readonly baseApiUrl = environment.apiUrl + 'registerplayer/';
+
+  loggedInPlayerSig = signal<LoggedInPlayer | null>(null);
+
+  registerPlayer(playerInput: RegisterPlayer): Observable<LoggedInPlayer | null> {
+    return this.http.post<LoggedInPlayer>(this.baseApiUrl + 'register', playerInput).pipe(
+      map(playerResponse => {
+        if (playerResponse) {
+          this.setCurrentPlayer(playerResponse);
+
+          this.navigateToReturnUrl(); // navigate to url wich player tried before log-in. else: default
+
+          return playerResponse;
+        }
+
+        return null;
+      })
+    );
+  }
+
+  loginPlayer(playerInput: LoginPlayer): Observable<LoggedInPlayer | null> {
+    return this.http.post<LoggedInPlayer>(this.baseApiUrl + 'login', playerInput).pipe(
+      map(playerResponse => {
+        if (playerResponse) {
+          this.setCurrentPlayer(playerResponse);
+
+          this.navigateToReturnUrl();
+
+          return playerResponse;
+        }
+
+        return null;
+      })  
+    );
+  }
+
+  /**
+   * Check if player's token is still valid or log them out.
+   * Called in app.component.ts
+   * @returns Observable<LoggedInPlayer | null>
+   */
+  authorizeLoggedInPlayer(): void {
+    this.http.get<ApiResponse>(this.baseApiUrl)
+      .pipe(
+        take(1))
+      .subscribe({
+        next: res => console.log(res.message),
+        error: err => {
+          console.log(err.error);
+          this.logOut()
+        }
+      });
+  }
+
+  setCurrentPlayer(loggedInPlayer: LoggedInPlayer): void {
+    this.setLoggedInPlayerRoles(loggedInPlayer);
+
+    this.loggedInPlayerSig.set(loggedInPlayer);
+
+    if (isPlatformBrowser(this.platformId)) // we make sure this code is ran on the browser and NOT server
+      localStorage.setItem('loggedInPlayer', JSON.stringify(loggedInPlayer));
+  }
+
+  setLoggedInPlayerRoles(loggedInPlayer: LoggedInPlayer): void {
+    loggedInPlayer.roles = []; // We have to initialize it before pushing. Otherwise, it's 'undefined' and push will not work.
+
+    const roles: string | string[] = JSON.parse(atob(loggedInPlayer.token.split('.')[1])).role; // get the token's 2nd part then select role
+
+    Array.isArray(roles) ? loggedInPlayer.roles = roles : loggedInPlayer.roles.push(roles);
+  }
+
+  logOut(): void {
+    this.loggedInPlayerSig.set(null);
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.clear(); // delete all browser's localStorage's items at once  
+    }
+
+    this.router.navigateByUrl('registerplayer/login');
+  }
+
+  private navigateToReturnUrl(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const returnUrl = localStorage.getItem('returnUrl');  
+      if (returnUrl)
+        this.router.navigate([returnUrl]);
+      else
+        this.router.navigate(['players']);
+
+      if (isPlatformBrowser(this.platformId)) // we make sure this code is ran on the browser and NOT server
+        localStorage.removeItem('returnUrl');
+    }
+  }
 }
