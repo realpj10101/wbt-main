ece856469cbef6d5b2a32c12825395fa62e3a7ce
Parsa Jafari <Parsajfr1385oo@gmail.com>
2024-10-28 23:09:59 +0330

    add vars, injects and auto run methods to register component.

--- client/src/app/components/account/register/register.component.ts
+++ client/src/app/components/account/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RegisterPlayer } from '../../../models/register-player.model';
 import { RegisterPlayerService } from '../../../services/register-player.service';
@@ -26,6 +26,29 @@ import { AutoFocusDirective } from '../../../directives/auto-focus.directive';
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy {
+  //#region enjects and vars
+  registerPlayerService = inject(RegisterPlayerService);
+  fb = inject(FormBuilder);
+
+  minDate = new Date();
+  maxDate = new Date();
+
+  passwordsNotMatch: boolean | undefined;
+  subscibedRegisterPlayer: Subscription | undefined;
+  emailExistError: string | undefined;
+  //#endregion
+
+  //#region auto-run-methods
+  ngOnDestroy(): void {
+    // set datePicker year limitations
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 99, 0, 1)
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  //#endregion
 
 }
