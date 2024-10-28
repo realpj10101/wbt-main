700445c07dfa647741e5ff99ce2354f0722e55f7
Parsa Jafari <Parsajfr1385oo@gmail.com>
2024-10-28 22:59:24 +0330

    add imports to register service

--- client/src/app/components/account/register/register.component.ts
+++ client/src/app/components/account/register/register.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RegisterPlayer } from '../../../models/register-player.model';
+import { RegisterPlayerService } from '../../../services/register-player.service';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { Subscription } from 'rxjs';
+import { AutoFocusDirective } from '../../../directives/auto-focus.directive';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, FormsModule, ReactiveFormsModule,
+    MatFormFieldModule, MatInputModule, MatButtonModule,
+    MatSnackBarModule, MatRadioModule,
+    MatDatepickerModule, MatNativeDateModule,
+    AutoFocusDirective
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
