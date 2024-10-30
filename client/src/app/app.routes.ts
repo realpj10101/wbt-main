ac5001883689a1d26dee232ab5fcc75820655370
Parsa Jafari <Parsajfr1385oo@gmail.com>
2024-10-30 16:21:06 +0330

    add routs

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/account/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'account/register', component: RegisterComponent}
+];
