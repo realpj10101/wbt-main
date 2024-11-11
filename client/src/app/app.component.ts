aa40b66c325d8739a3f6aba13895f724f27a28f6
Parsa <parsajfr1385oo@gmail.com>
2024-11-11 19:13:16 +0330

    debuging

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from "./components/account/register/register.component";
+import { FormBuilder, FormControl, ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    RegisterComponent
-],
+    ReactiveFormsModule, FormsModule,
+    MatFormFieldModule, MatInputModule, MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
- 
+  fB = Inject(FormBuilder);
+
+  registerFg = this.fB.group({
+    emailCtrl: ['']
+  })
 }
