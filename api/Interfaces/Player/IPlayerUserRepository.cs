46c642155e6c052ecf7d5bba6b5f6e873bcdce18
Parsa <parsajfr1385oo@gmail.com>
2025-01-20 13:45:51 +0330

    finish user management repo.

--- api/Interfaces/Player/IPlayerUserRepository.cs
+++ api/Interfaces/Player/IPlayerUserRepository.cs
@@ -2,5 +2,8 @@ namespace api.Repositories.Player;
 
 public interface IPlayerUserRepository
 {
+    public Task<AppUser?> GetByIdAsync(ObjectId playerId, CancellationToken cancellationToken);
+    public Task<AppUser?> GetPlayerByNameAsync(string userName, CancellationToken cancellationToken);
     public Task<ObjectId?> GetObjectIdByUserNameAsync(string userName, CancellationToken cancellationToken);
+    public Task<UpdateResult?> UpdatePlayerAsync(PlayerUpdateDto playerUpdateDto, string? hashedUserId, CancellationToken cancellationToken);
 }
--- api/Repositories/Player/PlayerUserRepository.cs
+++ api/Repositories/Player/PlayerUserRepository.cs
@@ -9,20 +9,36 @@ public class PlayerUserRepository : IPlayerUserRepository
     private readonly IMongoCollection<AppUser> _collection;
     private readonly ILogger<PlayerUserRepository> _logger;
     private readonly ITokenService _tokenService;
+    private readonly IPhotoService _photoService;
 
     public PlayerUserRepository(
         IMongoClient client, IMyMongoDbSettings dbSettings,
-        ILogger<PlayerUserRepository> logger, ITokenService tokenService)
+        ILogger<PlayerUserRepository> logger, ITokenService tokenService,
+        IPhotoService photoService)
     {
         var database = client.GetDatabase(dbSettings.DatabaseName);
         _collection = database.GetCollection<AppUser>(AppVariablesExtensions.collectionUsers);
         _logger = logger;
         _tokenService = tokenService;
+        _photoService = photoService;
     }
     
     #endregion
 
     #region User Management
+
+    public async Task<AppUser?> GetByIdAsync(ObjectId playerId, CancellationToken cancellationToken)
+    {
+        AppUser? appUser = await _collection.Find<AppUser>(player
+            => player.Id == playerId).SingleOrDefaultAsync(cancellationToken);
+
+        if (appUser is null) return null;
+
+        return appUser;
+    }
+    
+    public async Task<AppUser?> GetPlayerByNameAsync(string userName, CancellationToken cancellationToken) =>
+        await _collection.Find<AppUser>(appUser => appUser.NormalizedUserName == userName.ToUpper().Trim()).SingleOrDefaultAsync(cancellationToken);
 
     public async Task<ObjectId?> GetObjectIdByUserNameAsync(string userName, CancellationToken cancellationToken)
     {
@@ -33,6 +49,26 @@ public class PlayerUserRepository : IPlayerUserRepository
         
         return ValidationsExtensions.ValidateObjectId(playerId);
     }
+    
+    public async Task<UpdateResult?> UpdatePlayerAsync(PlayerUpdateDto playerUpdateDto, string? hashedUserId,
+        CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(hashedUserId)) return null;
 
+        ObjectId? playerId = await _tokenService.GetActualUserIdAsync(hashedUserId, cancellationToken);
+
+        if (playerId is null) return null;
+
+        UpdateDefinition<AppUser> updatePlayer = Builders<AppUser>.Update
+            .Set(appUser => appUser.Height, playerUpdateDto.Height)
+            .Set(appUser => appUser.KnownAs, playerUpdateDto.KnownAs)
+            .Set(appUser => appUser.LookingFor, playerUpdateDto.LookingFor)
+            .Set(appUser => appUser.City, playerUpdateDto.City)
+            .Set(appUser => appUser.Country, playerUpdateDto.Country);
+        
+        return await _collection.UpdateOneAsync<AppUser>(appUser => appUser.Id == playerId, updatePlayer, null, cancellationToken);
+    }
     #endregion
+    
+    
 }
