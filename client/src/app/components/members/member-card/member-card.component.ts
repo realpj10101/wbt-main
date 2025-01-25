578dce8d3f5c46347ce4f4c40fc3abb5e19b10d7
Parsa <parsajfr1385oo@gmail.com>
2025-01-25 13:17:20 +0330

    add member png and start creating member card component

--- client/src/app/components/members/member-card/member-card.component.ts
+++ client/src/app/components/members/member-card/member-card.component.ts
@@ -2,11 +2,19 @@ import { Component, inject, Input, Output } from '@angular/core';
 import { Member } from '../../../models/member.mode';
 import { environment } from '../../../../environments/environment.development';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-member-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, RouterModule, NgOptimizedImage,
+    MatCardModule, MatIconModule, MatButtonModule
+  ],
   templateUrl: './member-card.component.html',
   styleUrl: './member-card.component.scss'
 })
