8fccc39e1c7f87c4db2bfdf7b901655b71c0892f
Parsa <parsajfr1385oo@gmail.com>
2025-01-30 23:28:54 +0330

    Finish member card ts

--- client/src/app/components/members/member-card/member-card.component.ts
+++ client/src/app/components/members/member-card/member-card.component.ts
@@ -1,4 +1,4 @@
-  import { Component, EventEmitter, inject, Input, output, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, output, Output } from '@angular/core';
 import { Member } from '../../../models/member.model';
 import { environment } from '../../../../environments/environment.development';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -8,6 +8,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { FollowService } from '../../../services/follow.service';
+import { take } from 'rxjs';
+import { ApiResponse } from '../../../models/helpers/apiResponse.model';
 
 @Component({
   selector: 'app-member-card',
@@ -24,7 +26,43 @@ export class MemberCardComponent {
   @Output('unfollowUsernameOut') unfollowUserNameOut = new EventEmitter<string>();
   apiUrl = environment.apiUrl;
   private _followService = inject(FollowService);
-
-  apiUrl = environment.apiUrl;
   private _snack = inject(MatSnackBar);
+
+  follow(): void {
+    if (this.memberIn)
+      this._followService.create(this.memberIn?.userName).pipe(
+        take(1))
+        .subscribe({
+          next: (res: ApiResponse) => {
+            if (this.memberIn)
+              this.memberIn.isFollowing = true;
+
+            this._snack.open(res.message, 'close', {
+              duration: 7000,
+              horizontalPosition: 'center',
+              verticalPosition: 'top'
+            })
+          }
+        });
+  }
+
+  unfollow(): void {
+    if (this.memberIn)
+      this._followService.delete(this.memberIn.userName).pipe(
+        take(1))
+        .subscribe({
+          next: (res: ApiResponse) => {
+            if (this.memberIn) {
+              this.memberIn.isFollowing = false;
+              this.unfollowUserNameOut.emit(this.memberIn.userName);
+            }
+
+            this._snack.open(res.message, 'close', {
+              duration: 7000,
+              horizontalPosition: 'center',
+              verticalPosition: 'top'
+            })
+          }
+        });
+  }
 }
