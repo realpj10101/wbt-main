9b7d513150eb6a7eb1484f01beee105fc592361c
Parsa <parsajfr1385oo@gmail.com>
2025-01-30 23:07:41 +0330

    Start creating member card

--- client/src/app/components/members/member-card/member-card.component.ts
+++ client/src/app/components/members/member-card/member-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, Output } from '@angular/core';
+  import { Component, EventEmitter, inject, Input, output, Output } from '@angular/core';
 import { Member } from '../../../models/member.model';
 import { environment } from '../../../../environments/environment.development';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { FollowService } from '../../../services/follow.service';
 
 @Component({
   selector: 'app-member-card',
@@ -20,6 +21,9 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class MemberCardComponent {
   @Input('memberInput') memberIn: Member | undefined;
+  @Output('unfollowUsernameOut') unfollowUserNameOut = new EventEmitter<string>();
+  apiUrl = environment.apiUrl;
+  private _followService = inject(FollowService);
 
   apiUrl = environment.apiUrl;
   private _snack = inject(MatSnackBar);
