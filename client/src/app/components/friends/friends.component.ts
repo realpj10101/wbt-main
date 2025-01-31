95020eb548fb1754cc6e30193aa2a233cc66bb84
Parsa <parsajfr1385oo@gmail.com>
2025-01-31 14:47:05 +0330

    finish friends component ts

--- client/src/app/components/friends/friends.component.ts
+++ client/src/app/components/friends/friends.component.ts
@@ -1,20 +1,80 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FollowService } from '../../services/follow.service';
 import { Member } from '../../models/member.model';
 import { Pagination } from '../../models/helpers/pagination.model';
+import { FollowParams } from '../../models/helpers/follow-params.model';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { PaginatedResult } from '../../models/helpers/pagination-result.model';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
+import { MemberCardComponent } from '../members/member-card/member-card.component';
+import { FollowPredicate } from '../../enums/follow-predicate-enum';
 
 @Component({
   selector: 'app-friends',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MemberCardComponent,
+    MatTabsModule, MatPaginatorModule
+  ],
   templateUrl: './friends.component.html',
   styleUrl: './friends.component.scss'
 })
-export class FriendsComponent {
+export class FriendsComponent implements OnInit {
   followService = inject(FollowService);
   members: Member[] | undefined;
   pagination: Pagination | undefined;
   readonly followings = 'Followings';
-  // followParams
+  followParams = new FollowParams();
 
+  pageSizeOption = [3, 9, 12];
+  pageEvent: PageEvent | undefined;
+
+  ngOnInit(): void {
+
+  }
+
+  getAll(): void {
+    this.members = [];
+
+    this.followService.getAll(this.followParams).subscribe({
+      next: (res: PaginatedResult<Member[]>) => {
+        if (res.body && res.pagination) {
+          this.members = res.body;
+          this.pagination = res.pagination;
+        }
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  onTabChange(event: MatTabChangeEvent) {
+    if (event.tab.textLabel === this.followings) {
+      this.followParams.predicate = FollowPredicate.FOLLOWINGS;
+
+      this.getAll();
+    }
+    else {
+      this.followParams.predicate = FollowPredicate.FOLLOWERS;
+
+      this.getAll();
+    }
+  }
+
+  removeUnfollowedFromMembers(userName: string): void {
+    const members = this.members?.filter(member => member.userName !== userName);
+    this.members = members;
+  }
+
+  handlePageEvent(e: PageEvent) {
+    if (e.pageSize !== this.followParams.pageSize)
+      e.pageIndex = 0;
+
+    this.pageEvent = e;
+    this.followParams.pageSize = e.pageSize;
+    this.followParams.pageNumber = e.pageIndex + 1;
+
+    this.getAll();
+  }
 }
