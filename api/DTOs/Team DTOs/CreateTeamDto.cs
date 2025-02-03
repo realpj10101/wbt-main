205a300b896064b1dae46a77047ce5613c1e45df
Parsa <parsajfr1385oo@gmail.com>
2025-02-03 12:25:18 +0330

    Add more properties to CreateTeamDto.cs and create

--- api/DTOs/Team DTOs/CreateTeamDto.cs
+++ api/DTOs/Team DTOs/CreateTeamDto.cs
@@ -1,5 +1,21 @@
 namespace api.DTOs.Team_DTOs;
 
 public record CreateTeamDto(
-    string TeamName
+    string TeamName,
+    string TeamLevel,
+    string Achievements,
+    int GamesPlayed,
+    int GamesWon,
+    int GamesLost,
+    DateTime CreatedAt
     );
+
+public class ShowTeamDto
+{
+    public string TeamName { get; init; } = string.Empty;
+    public string TeamLevel { get; init; } = string.Empty;
+    public string Achievements { get; init; } = string.Empty;
+    public int GamesPlayed { get; init; }
+    public int GamesWon { get; init; }
+    public DateTime CreatedAt { get; init; }
+}
