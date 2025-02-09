06942cfc84f42883d8a58d35006650f0cf85ac70
Parsa <parsajfr1385oo@gmail.com>
2025-02-09 11:43:20 +0330

    Create get Http params mehtod and add orderBy to the method params and use it in getAll method

--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -3,7 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { PaginationHandler } from '../extension/paginationHandler';
 import { MemberParams } from '../models/helpers/member-params.model';
-import { Observable } from 'rxjs';
+import { empty, Observable } from 'rxjs';
 import { PaginatedResult } from '../models/helpers/pagination-result.model';
 import { Member } from '../models/member.model';
 import { P } from '@angular/cdk/keycodes';
@@ -18,17 +18,24 @@ export class MemberService {
   private paginationHandler = new PaginationHandler();
 
   getAll(memberParams: MemberParams): Observable<PaginatedResult<Member[]>> {
-    let params = new HttpParams();
-
-    if (memberParams) {
-      params = params.append('pageNumber', memberParams.pageNumber);
-      params = params.append('pageSize', memberParams.pageSize);
-    }
+    const params = this.getHttpParams(memberParams);
 
     return this.paginationHandler.getPaginatedResult<Member[]>(this._baseApiUrl, params);
   }
 
   getByUserName(userNameInput: string): Observable<Member | undefined> {
     return this.http.get<Member>(this._baseApiUrl + 'get-by-username/' + userNameInput);
+  }
+
+  private getHttpParams(memberParams: MemberParams): HttpParams {
+    let params = new HttpParams();
+
+    if (memberParams) {
+      params = params.append('pageSize', memberParams.pageSize);
+      params = params.append('pageNumebr', memberParams.pageNumber);
+      params = params.append('orderBy', memberParams.orderBy);
+    }
+    
+    return params;
   }
 }
