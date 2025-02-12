33b5613747a2d152fb817a65fbc5f1760b885353
Parsa <parsajfr1385oo@gmail.com>
2025-02-12 13:07:43 +0330

    Add test player dto

--- api/DTOs/Player DTOs/TestPlayer.cs
+++ api/DTOs/Player DTOs/TestPlayer.cs
@@ -2,5 +2,6 @@ namespace api.DTOs;
 
 public record TestPlayer(
     [MaxLength(50)]string? Name,
-    [MaxLength(50)]string? LastName    
+    [MaxLength(50)]string? LastName
+    // string Gender
     );
