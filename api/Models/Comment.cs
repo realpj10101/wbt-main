0eb44454675be263881cfc0058e978ecf943f539
Parsa <parsajfr1385oo@gmail.com>
2025-02-15 11:46:09 +0330

    Add two properties to Comment.cs

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -6,6 +6,8 @@ public record Comment(
     [Optional][property: BsonId, BsonRepresentation(BsonType.ObjectId)] ObjectId Id, // optional
     ObjectId CommenterId,
     ObjectId CommentedMemberId,
+    string CommenterName,
+    string CommentedMemberName,
     string Content,
     DateTime CreatedAt
     );
