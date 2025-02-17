66b81de9c058b1302ee15a061cc65e0c76eb86c7
Parsa <parsajfr1385oo@gmail.com>
2025-02-17 22:41:15 +0330

    generate team details

---
+++ client/src/app/components/teams/team-details/team-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-details.component.html',
+  styleUrl: './team-details.component.scss'
+})
+export class TeamDetailsComponent {
+
+}
