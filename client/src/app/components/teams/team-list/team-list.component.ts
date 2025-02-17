44e02af25f943e5d59d262293e9ac9b41b29c53e
Parsa <parsajfr1385oo@gmail.com>
2025-02-17 22:40:48 +0330

    generate team list component

---
+++ client/src/app/components/teams/team-list/team-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-list.component.html',
+  styleUrl: './team-list.component.scss'
+})
+export class TeamListComponent {
+
+}
