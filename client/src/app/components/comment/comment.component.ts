5d3e09bf9e56ea26a58697b6ace9a9ef1057a53d
Parsa <parsajfr1385oo@gmail.com>
2025-02-18 14:18:05 +0330

    add material forms and input and debug add comment

--- client/src/app/components/comment/comment.component.ts
+++ client/src/app/components/comment/comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { CommentService } from '../../services/comment.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Member } from '../../models/member.model';
@@ -15,9 +15,10 @@ import { IntlModule } from "angular-ecmascript-intl";
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from "@angular/material/tabs";
 import { UserComment } from '../../models/user-comment.model';
-import { matExpansionAnimations, MatExpansionModule } from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { CommonModule } from '@angular/common';
-import { P } from '@angular/cdk/keycodes';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-comment',
@@ -26,13 +27,15 @@ import { P } from '@angular/cdk/keycodes';
     CommonModule,
     FormsModule, ReactiveFormsModule,
     MatIconModule, MatButtonModule, MatTabsModule,
-    IntlModule, MatExpansionModule
+    IntlModule, MatExpansionModule, MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
 export class CommentComponent implements OnInit {
   @Input('memberInput') memberInput: Member | undefined;
+  @Output('removeCommentOut') removeCommentOut = new EventEmitter<string>();
   private _commentService = inject(CommentService);
   private _snack = inject(MatSnackBar);
   private _fb = inject(FormBuilder);
@@ -57,10 +60,10 @@ export class CommentComponent implements OnInit {
   }
 
   getMember(): void {
-    const userName: string | null = this._route.snapshot.paramMap.get('userName');
+    // const userName: string | null = this._route.snapshot.paramMap.get('userName');
 
-    if (userName)
-      this._memberService.getByUserName(userName)
+    if (this.userName)
+      this._memberService.getByUserName(this.userName)
         .pipe(
           take(1))
         .subscribe({
@@ -81,6 +84,7 @@ export class CommentComponent implements OnInit {
       .pipe(take(1))
       .subscribe({
         next: (res: ApiResponse) => {
+          this.getComments();
           if(this.userName && this.memberInput?.userName) {
 
             const newComment: UserComment  = {
@@ -89,6 +93,7 @@ export class CommentComponent implements OnInit {
               content: commetnIn.content,
               createdAt: new Date()
             }
+            console.log(newComment); 
             this.comments?.push(newComment);
           }
             
@@ -101,6 +106,25 @@ export class CommentComponent implements OnInit {
       });
 
     console.log('com compo')
+  }
+
+  remove(): void {
+    if (this.memberInput)
+      this._commentService.remove(this.memberInput.userName).pipe(
+        take(1))
+        .subscribe({
+          next: (res: ApiResponse) => {
+            if (this.memberInput) {
+              this.removeCommentOut.emit(this.memberInput?.userName);
+            }
+
+            this._snack.open(res.message, 'close', {
+              duration: 7000,
+              horizontalPosition: 'center',
+              verticalPosition: 'top'
+            })
+          }
+        })
   }
 
   getComments(): void {
