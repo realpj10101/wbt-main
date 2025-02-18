baec95a2af1c31bdbbf6cafd5eafc2d99321c8e4
Parsa <parsajfr1385oo@gmail.com>
2025-02-18 16:35:53 +0330

    create likes component

--- client/src/app/components/likes/likes.component.ts
+++ client/src/app/components/likes/likes.component.ts
@@ -1,6 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { LikeService } from '../../services/like.service';
 import { Member } from '../../models/member.model';
+import { Pagination } from '../../models/helpers/pagination.model';
+import { LikeParams } from '../../models/helpers/like-params.model';
+import { PageEvent } from '@angular/material/paginator';
+import { PaginatedResult } from '../../models/helpers/pagination-result.model';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { LikePredicate } from '../../enums/like-predicate-enum';
 
 @Component({
   selector: 'app-likes',
@@ -9,8 +15,62 @@ import { Member } from '../../models/member.model';
   templateUrl: './likes.component.html',
   styleUrl: './likes.component.scss'
 })
-export class LikesComponent {
+export class LikesComponent implements OnInit {
   private _likeService = inject(LikeService);
   members: Member[] | undefined;
-  // paginatio
+  pagination: Pagination | undefined;
+  readonly likings = 'Likings';
+  likeParams = new LikeParams();
+
+  pageSizeOption = [3, 9, 12];
+  pageEvent: PageEvent | undefined;
+
+  ngOnInit(): void {
+      this.getAll();
+  }
+
+  getAll(): void {
+    this.members = [];
+
+    this._likeService.getAll(this.likeParams).subscribe({
+      next: (res: PaginatedResult<Member[]>) => {
+        if (res.body && res.pagination) {
+          this.members = res.body;
+          this.pagination = res.pagination;
+        }
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  onTabChange(event: MatTabChangeEvent) {
+    if (event.tab.textLabel == this.likings) {
+      this.likeParams.predicate = LikePredicate.LIKINGS;
+
+      this.getAll();
+    }
+    else {
+      this.likeParams.predicate = LikePredicate.LIKERS;
+
+      this.getAll();
+    }
+  }
+
+  removeDislikedUserFromMembers(userName: string): void {
+    const members = this.members?.filter(member => member.userName !== userName);
+    this.members = members;
+  }
+
+  handlePageEvent(e: PageEvent): void {
+    if (e.pageSize !== this.likeParams.pageSize)
+      e.pageIndex = 0;
+
+    this.pageEvent = e;
+    this.likeParams.pageSize = e.pageSize;
+    this.likeParams.pageNumber = e.pageIndex + 1;
+
+    this.getAll();
+  }
+
+
 }
---
+++ client/src/app/enums/like-predicate-enum.ts
@@ -0,0 +1,4 @@
+export enum LikePredicate {
+    LIKINGS,
+    LIKERS
+}
