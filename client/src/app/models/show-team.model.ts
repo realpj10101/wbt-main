9c538edc51e1dd11550318dfc9bc1b9ba3707129
Parsa <parsajfr1385oo@gmail.com>
2025-02-20 16:54:41 +0330

    Create show team model

---
+++ client/src/app/models/show-team.model.ts
@@ -0,0 +1,10 @@
+export interface ShowTeam {
+    teamName: string;
+    membersUserNames: string[];
+    teamLevel: string;
+    achievements: string;
+    gamesPlayed: number;
+    gamesWon: number;
+    gamesLost: number;
+    createdAt: Date;
+}
