f343dfb5189798c5c440d36bc0efec3abf68da5f
Parsa <parsajfr1385oo@gmail.com>
2025-02-20 19:56:19 +0330

    Create team registration

--- client/src/app/components/create-team/create-team.component.ts
+++ client/src/app/components/create-team/create-team.component.ts
@@ -7,12 +7,16 @@ import { CreateTeam } from '../../models/create.team.model';
 import { ShowTeam } from '../../models/show-team.model';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-create-team',
   standalone: true,
   imports: [
-    FormsModule, ReactiveFormsModule
+    FormsModule, ReactiveFormsModule,
+    MatFormFieldModule, MatInputModule, MatButtonModule,
+    MatStepperModule
   ],
   templateUrl: './create-team.component.html',
   styleUrl: './create-team.component.scss'
@@ -23,37 +27,40 @@ export class CreateTeamComponent {
   private _fb = inject(FormBuilder);
   private _route = inject(ActivatedRoute);
 
-  teamFg: FormGroup = this._fb.group({
-    teamNameCtrl: ['', [Validators.maxLength(50)]],
-    teamLevelCtrl: ['', [Validators.maxLength(20)]],
-    achievementsCtrl: ['', [Validators.maxLength(50)]],
-    gamesPlayedCtrl: '',
-    gamesWonCtrl: '',
-    gamesLostCtrl: ''
+  firstFg: FormGroup = this._fb.group({
+    teamNameCtrl: ['', [Validators.required, Validators.maxLength(50)]],
+    teamLevelCtrl: ['', [Validators.required, Validators.maxLength(20)]],
+    achievementsCtrl: ['', [Validators.required, Validators.maxLength(50)]]
+  });
+
+  secFg: FormGroup = this._fb.group({
+    gamesPlayedCtrl: ['', [Validators.required]],
+    gamesWonCtrl: ['', [Validators.required]],
+    gamesLostCtrl: ['', [Validators.required]]
   });
 
   get TeamNameCtrl(): FormControl {
-    return this.teamFg.get('teamNameCtrl') as FormControl;
+    return this.firstFg.get("teamNameCtrl") as FormControl;
   }
 
   get TeamLevelCtrl(): FormControl {
-    return this.teamFg.get('teamLevelCtrl') as FormControl;
+    return this.firstFg.get('teamLevelCtrl') as FormControl;
   }
 
   get AchievementsCtrl(): FormControl {
-    return this.teamFg.get('achievementsCtrl') as FormControl;
+    return this.firstFg.get('achievementsCtrl') as FormControl;
   }
 
   get GamesPlayedCtrl(): FormControl {
-    return this.teamFg.get('gamesPlayedCtrl') as FormControl;
+    return this.secFg.get('gamesPlayedCtrl') as FormControl;
   }
 
   get GamesWonCtrl(): FormControl {
-    return this.teamFg.get('gamesWonCtrl') as FormControl;
+    return this.secFg.get('gamesWonCtrl') as FormControl;
   }
 
   get GamesLostCtrl(): FormControl {
-    return this.teamFg.get('gamesLostCtrl') as FormControl;
+    return this.secFg.get('gamesLostCtrl') as FormControl;
   }
 
   create(): void {
