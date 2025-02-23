31d7c193da36e09123c2c543d899808cb79e6074
Parsa <parsajfr1385oo@gmail.com>
2025-02-23 10:45:34 +0330

    Add search property to member params

--- api/Helpers/MemberParams.cs
+++ api/Helpers/MemberParams.cs
@@ -4,4 +4,5 @@ public class MemberParams : PaginationParams
 {
     public ObjectId? UserId { get; set; }
     public string OrderBy { get; set; } = "lastActive";
+    public string Search { get; set; } = String.Empty;
 }
