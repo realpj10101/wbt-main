6b7ad880ff3093cd23519e488bd0682e612a8e99
Parsa <parsajfr1385oo@gmail.com>
2025-02-27 21:55:30 +0330

    add some methods to coach account service

--- client/src/app/services/coach-account.service.ts
+++ client/src/app/services/coach-account.service.ts
@@ -4,8 +4,10 @@ import { Router } from '@angular/router';
 import { LoggedInPlayer } from '../models/logged-in-player.model';
 import { environment } from '../../environments/environment.development';
 import { RegisterPlayer } from '../models/register-player.model';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { LoginPlayer } from '../models/login-player.model';
+import { ApiResponse } from '../models/helpers/apiResponse.model';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +26,50 @@ export class CoachAccountService {
 
   loginCoach(userInput: LoginPlayer): Observable<LoggedInPlayer | null> {
     return this.http.post<LoggedInPlayer>(this._apiUrl + 'login', userInput);
+  }
+
+  authorizeLoggedInCoach(): void {
+    this.http.get<ApiResponse>(this._apiUrl)
+      .pipe(
+        take(1))
+        .subscribe({
+          next: res => {
+            if (res.message)
+              console.log(res.message);
+          },
+          error: err => {
+            console.log(err.error);
+
+          }
+        })
+  }
+
+  setLoggedInCoachRoles(loggedInUser: LoggedInPlayer): void {
+    loggedInUser.roles = []; // We have to initialize
+
+    const roles: string | string[] = JSON.parse(atob(loggedInUser.token.split('.')[1])).role;
+  }
+
+  logOut(): void {
+    this.loggedInCoachSig.set(null);
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.clear();
+    }
+
+  this.router.navigateByUrl('coach-account/login');
+  }
+
+  private navigateToReturnUrl(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const returnUrl = localStorage.getItem('returnUrl');
+      if (returnUrl)
+        this.router.navigate([returnUrl]);
+      else
+        this.router.navigate(['members']);
+
+      if (isPlatformBrowser(this.platformId)) // we make sure this code is ran on the browser and NOT server
+        localStorage.removeItem('returnUrl');
+    }
   }
 }
