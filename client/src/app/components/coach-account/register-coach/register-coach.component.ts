76f58d40ebde3f805cf61f8d40f65390b6a8bd8a
Parsa <parsajfr1385oo@gmail.com>
2025-02-28 10:18:36 +0330

    finish register coach component ts

--- client/src/app/components/coach-account/register-coach/register-coach.component.ts
+++ client/src/app/components/coach-account/register-coach/register-coach.component.ts
@@ -1,12 +1,118 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterLink } from '@angular/router';
+import { AutoFocusDirective } from '../../../directives/auto-focus.directive';
+import { CoachAccountService } from '../../../services/coach-account.service';
+import { Subscription } from 'rxjs';
+import { RegisterPlayer } from '../../../models/register-player.model';
 
 @Component({
   selector: 'app-register-coach',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, FormsModule, ReactiveFormsModule,
+    MatFormFieldModule, MatInputModule, MatButtonModule,
+    MatSnackBarModule, MatRadioModule,
+    MatDatepickerModule, MatNativeDateModule,
+    AutoFocusDirective, RouterLink
+  ],
   templateUrl: './register-coach.component.html',
   styleUrl: './register-coach.component.scss'
 })
-export class RegisterCoachComponent {
+export class RegisterCoachComponent implements OnInit, OnDestroy {
+  coachAccountService = inject(CoachAccountService);
+  fb = inject(FormBuilder);
 
+  minDate = new Date(); //yyyy/mm/dd/ss
+  maxDate = new Date();
+
+  passwordsNotMatch: boolean | undefined;
+  subscribedRegisterCoach: Subscription | undefined;
+  emailExistError: string | undefined;
+
+  ngOnInit(): void {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 99, 0, 1);
+    this.maxDate = new Date(currentYear - 6, 0, 1);
+  }
+
+  ngOnDestroy(): void {
+    this.subscribedRegisterCoach?.unsubscribe();
+  }
+
+  registerFg = this.fb.group({
+    genderCtrl: ['female', [Validators.required]],
+    emailCtrl: ['', [Validators.required, Validators.maxLength(50), Validators.pattern(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$/)]],
+    userNameCtrl: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+    dateOfBirthCtrl: ['', [Validators.required]],
+    passwordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]],
+    confirmPasswordCtrl: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(20)]]
+  })
+
+  get GenderCtrl(): FormControl {
+    return this.registerFg.get('genderCtrl') as FormControl;
+  }
+
+  get EmailCtrl(): FormControl {
+    return this.registerFg.get('emailCtrl') as FormControl;
+  }
+
+  get UserNameCtrl(): FormControl {
+    return this.registerFg.get('userNameCtrl') as FormControl;
+  }
+
+  get DateOfBirthCtrl(): FormControl {
+    return this.registerFg.get('dateOfBirthCtrl') as FormControl;
+  }
+
+  get PasswordCtrl(): FormControl {
+    return this.registerFg.get('passwordCtrl') as FormControl;
+  }
+
+  get ConfirmPasswordCtrl(): FormControl {
+    return this.registerFg.get('confirmPasswordCtrl') as FormControl;
+  }
+
+  register(): void {
+    const dob: string | undefined = this.getDateOnly(this.DateOfBirthCtrl.value);
+
+    if (this.PasswordCtrl.value === this.ConfirmPasswordCtrl.value) {
+      this.passwordsNotMatch = false;
+
+      let registerCoach: RegisterPlayer = {
+        gender: this.GenderCtrl.value,
+        email: this.EmailCtrl.value,
+        userName: this.UserNameCtrl.value,
+        dateOfBirth: this.DateOfBirthCtrl.value,
+        password: this.PasswordCtrl.value,
+        confirmPassword: this.PasswordCtrl.value,
+      }
+
+      this.subscribedRegisterCoach = this.coachAccountService.registerCoach(registerCoach).subscribe({
+        next: coach => console.log(coach),
+        error: err => this.emailExistError = err.error
+      })
+
+      console.log(this.subscribedRegisterCoach);
+    }
+    else {
+      this.passwordsNotMatch = true;
+    }
+  }
+
+  getDateOnly(dob: string | null): string | undefined {
+    if (!dob) return undefined;
+
+    let theDob: Date = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes() - theDob.getTimezoneOffset())).toISOString().slice(0, 10);
+  }
 }
+
