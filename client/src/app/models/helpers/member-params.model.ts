b3e3be34ef95e2d1c2a86be8d023f86ebae3e5ed
Parsa <parsajfr1385oo@gmail.com>
2025-03-01 13:50:31 +0330

    Add gender property to member params

--- client/src/app/models/helpers/member-params.model.ts
+++ client/src/app/models/helpers/member-params.model.ts
@@ -5,4 +5,10 @@ export class MemberParams extends PaginationParams {
     orderBy = 'lastAcive';
     minAge = 6;
     maxAge = 99;
+    gender: string | undefined;
+
+    constructor(gender: string) {
+        super(); // Constructors of derived classes must contain a 'super' call to properly set up the inheritance chain.
+        this.gender = gender === 'female' ? 'male' : 'female';
+    }
 }
