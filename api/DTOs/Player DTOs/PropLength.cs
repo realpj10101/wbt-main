3b19cd91391258ef30dc1723bf2371f54455aa65
Parsa <parsajfr1385oo@gmail.com>
2025-03-17 14:50:16 +0330

    create prop length

---
+++ api/DTOs/Player DTOs/PropLength.cs
@@ -0,0 +1,10 @@
+namespace api.DTOs;
+
+public static class PropLength
+{
+    internal const int EmailMaxLength = 100;
+    internal const int UserNameMinLength = 1;
+    internal const int UserNameMaxLength = 50;
+    internal const int PasswordsMinLength = 8;
+    internal const int PasswordsMaxLength = 50;
+}
