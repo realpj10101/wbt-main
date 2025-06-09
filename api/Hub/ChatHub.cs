38740c9e1a99bb53afa1af41432173cf1b96b93f
realpj10101 <parsajfr1385oo@gmail.com>
2025-06-09 12:36:21 +0330

    add on connected and on disconnected methods to chat hub

--- api/Hub/ChatHub.cs
+++ api/Hub/ChatHub.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using api.Interfaces;
 using api.Interfaces.Teams;
 using api.Models;
@@ -8,11 +9,40 @@ namespace api.Hub;
 public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
 {
     private ITeamMessagingRepository _teamMessagingRepository;
+
+    private static readonly ConcurrentDictionary<string, string> _onlineUsers = new();
     public ChatHub(ITeamMessagingRepository teamMessagingRepository)
     {
         _teamMessagingRepository = teamMessagingRepository;
     }
+    
+    public override async Task OnConnectedAsync()
+    {
+        string userName = Context.User?.Identity?.Name ?? Context.ConnectionId;
 
+        _onlineUsers[userName] = Context.ConnectionId;
+
+        // Notify clients that a user came online
+        await Clients.All.SendAsync("UserOnline", userName);
+
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var user = _onlineUsers.FirstOrDefault(x => x.Value == Context.ConnectionId);
+        if (!string.IsNullOrEmpty(user.Key))
+        {
+            _onlineUsers.TryRemove(user.Key, out _);
+            
+            // Store last seen time
+            var lastSeen = DateTime.UtcNow;
+
+            await Clients.All.SendAsync("UserOffline", user.Key, lastSeen);
+        }
+
+        await base.OnDisconnectedAsync(exception);
+    }
     public async Task SendMessage(string userName, string message, string teaName)
     {
         MessageSenderDto sender = new(
@@ -23,5 +53,10 @@ public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
         await _teamMessagingRepository.SavedMessageAsync(sender, teaName);
 
         await Clients.All.SendAsync("ReceiveMessage", userName, message);
+    }
+
+    public Task<List<string>> GetOnlineUsers()
+    {
+        return Task.FromResult(_onlineUsers.Keys.ToList());
     }
 }
