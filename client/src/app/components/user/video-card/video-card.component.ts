87e7a8c7c5e2433ff0711f32ffa83fb101bd7751
realpj10101 <parsajfr1385oo@gmail.com>
2025-09-20 15:29:10 +0330

    Added methods for video buffer time

--- client/src/app/components/user/video-card/video-card.component.ts
+++ client/src/app/components/user/video-card/video-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, Input, OnDestroy, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -11,7 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './video-card.component.html',
   styleUrl: './video-card.component.scss'
 })
-export class VideoCardComponent {
+export class VideoCardComponent implements OnDestroy {
   @Input() videoSrc: string | undefined;
   @Input() posterSrc: string | undefined;
   @Input() alt = 'preview';
@@ -39,6 +39,15 @@ export class VideoCardComponent {
 
   @ViewChild('player') playerRef!: ElementRef<HTMLVideoElement>;
   @ViewChild('thumbVideo') thumbVideoRef!: ElementRef<HTMLVideoElement>;
+  @ViewChild('seekWrap') seekWrapRef!: ElementRef<HTMLElement>;
+  @ViewChild('thumbCanvas') thumbCanvasRef?: ElementRef<HTMLCanvasElement>;
+
+  ngOnDestroy(): void {
+    // document.removeEventListener('mousemove', _onDocMove);
+    // document.removeEventListener('mousemove', _onDocUp);
+    // document.removeEventListener('mousemove', _onDocTouchMove);
+    // document.removeEventListener('mousemove', _onDocTouchEnd);
+  }
 
   togglePlay(): void {
     const video = this.playerRef.nativeElement;
@@ -71,13 +80,40 @@ export class VideoCardComponent {
     const video = this.playerRef.nativeElement;
 
     this.duration = video.duration || 0;
+
+    const thumbVideo = this.thumbVideoRef.nativeElement;
+    if (thumbVideo.readyState >= 1) this._thumbMetaReady = true;
+    else {
+      thumbVideo.addEventListener('loadedmetadata', () => { this._thumbMetaReady = true; }, { once: true });
+    }
   }
 
   onTimeUpdate(): void {
-    if (!this.seeking) {
-      this.currentTime = this.playerRef.nativeElement.currentTime || 0;
+    const v = this.playerRef.nativeElement;
+
+    this.currentTime = v.currentTime || 0;
+  }
+
+  onProgress(): void {
+    const video = this.playerRef.nativeElement;
+
+    try {
+      if (video.buffered.length) {
+        const end = video.buffered.end(video.buffered.length - 1); // last buffer
+        this.bufferPercent = this.duration ? Math.min((end / this.duration) * 100, 100) : 0;
+      }
+      else {
+        this.bufferPercent = 0;
+      }
+    }
+    catch {
+      this.bufferPercent = 0;
     }
   }
+
+  // onSeekHover(event: MouseEvent) {
+  //   const { x, time, clampedX } = 
+  // }
 
   onSeekInput(event: Event): void {
     const value = Number((event.target as HTMLInputElement).value);
